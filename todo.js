659f48afd51111a23698f8062d6c5a974d1a9a3c
tienlam <lamtien98@gmail.com>
2022-10-19 17:32:14 +0700

    c08.2

--- todo.js
+++ todo.js
@@ -26,9 +26,19 @@ function newElement() {
 chucNangChecked();
 
 function deleteTask() {
-    let closebtn = document.createElement("span");
-    let closeTextNode = document.createTextNode("x");
-    closebtn.appendChild(closeTextNode);
+    // tạo thẻ span
+    let span = document.createElement("span");
+    // tạo text "x"
+    let spanText = document.createTextNode("x");
+    span.appendChild(spanText);
+    let li = document.getElementsByTagName("li");
+    let ul = document.getElementById("myUL");
+    let mangLi = ul.children;
+    for (let i = 0; i < mangLi.length; i++) {
+        li[i].appendChild(span);
+    }
+    // let closeTextNode = document.createTextNode("x");
+    // closebtn.appendChild(closeTextNode);
     
 }
 deleteTask();
